0bbe3f7a683bf9452640c53c75247d5ea648b340
Alan Cruickshank <alanmcruickshank@gmail.com>
2018-10-15 22:13:28 +0100

    reset steps per mm on z to not use microstepping

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -611,7 +611,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 3200, 100 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 100, 100 }
 
 /**
  * Default Max Feed Rate (mm/s)
