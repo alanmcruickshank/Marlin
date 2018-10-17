9ff909510182a073a900c0ba69672719b268a903
Alan Cruickshank <alanmcruickshank@gmail.com>
2018-10-17 01:58:01 +0100

    Changes Working

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -611,14 +611,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 100, 100 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 3200, 100 }
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 300, 300, 2, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 8, 25 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
