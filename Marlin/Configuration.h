cd5731e36d0ae4533316f0bdbd9e80b57de5a388
Alan Cruickshank <alan@designingoverload.com>
2023-04-01 14:05:55 +0100

    Move to smaller pulley on Z

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1168,7 +1168,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 800, 100 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 1600, 100 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
